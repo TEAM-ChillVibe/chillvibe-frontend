9be6199503f29755687ab656babb037d1e4e58db
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-07-28 18:32:54 +0900

    design: base container 여백/간격 수정

--- src/components/layout/BaseContainer.jsx
+++ src/components/layout/BaseContainer.jsx
@@ -9,10 +9,11 @@ const BaseContainer = ({ children, ...props }) => {
         flexDirection: 'column', // 자식 요소들을 세로 방향으로 나열
         justifyContent: 'center', // 수평 중앙 정렬
         alignItems: 'center', // 수직 중앙 정렬
-        mx: '15%',
+        mx: '20%',
         my: '60px',
         px: '16px',
         py: '24px',
+        gap: 4,
       }}
     >
       {children}
