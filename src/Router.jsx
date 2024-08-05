f934637e038e9615e448d7446c0db9bcfcf6bd83
[Cloud3] 전재윤 <nick92641@gmail.com>
2024-08-05 15:02:56 +0900

    fix : 회원가입에 소개 항목 제거

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,6 +10,14 @@ const Login = lazy(() => import('./pages/auth/login/Login'));
 const Signup = lazy(() => import('./pages/auth/signup/Signup'));
 const EditProfile = lazy(() => import('./pages/mypage/EditProfile'));
 const Search = lazy(() => import('./pages/search/SearchPage'));
+const Forbidden = lazy(() => import('./pages/error/code/Forbidden'));
+const InternalServerError = lazy(
+  () => import('./pages/error/code/InternalServerError'),
+);
+const NotFound = lazy(() => import('./pages/error/code/NotFound'));
+const ServiceUnavailable = lazy(
+  () => import('./pages/error/code/ServiceUnavailable'),
+);
 
 const Router = () => {
   return (
@@ -36,6 +44,10 @@ const Router = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/403" element={<Forbidden />} />
+        <Route path="/500" element={<InternalServerError />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/503" element={<ServiceUnavailable />} />
       </Routes>
     </Suspense>
   );
