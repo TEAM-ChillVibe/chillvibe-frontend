c8818dc0490ccf36f8b57a95b8faad85e2e688fb
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-08-09 18:55:43 +0900

    design: mycomment ui 수정

--- src/pages/mypage/tabs/MyComment.jsx
+++ src/pages/mypage/tabs/MyComment.jsx
@@ -59,7 +59,7 @@ const MyComment = () => {
 
   // 댓글 클릭 핸들러
   const handleCommentClick = postId => {
-    navigate(`/api/posts/${postId}`); // 해당 게시글 페이지로 이동
+    navigate(`/posts/${postId}`); // 해당 게시글 페이지로 이동
   };
 
   // 페이지 인덱스 계산
@@ -86,7 +86,8 @@ const MyComment = () => {
               <Box
                 key={comment.id}
                 sx={{
-                  py: 2,
+                  py: 2.5,
+                  width: '100%',
                   display: 'flex',
                   alignItems: 'flex-start',
                   justifyContent: 'space-between',
@@ -123,14 +124,21 @@ const MyComment = () => {
                     )}
                   </Typography>
                 </Box>
-                <Box>
+                <Box
+                  sx={{
+                    width: 120,
+                    height: 120,
+                    ml: 2,
+                    borderRadius: 2,
+                    overflow: 'hidden',
+                  }}
+                >
                   <img
                     src={comment.postTitleImageUrl}
                     alt={comment.postTitle}
                     style={{
-                      width: 100,
-                      height: 100,
-                      borderRadius: 2,
+                      width: '100%',
+                      height: '100%',
                       objectFit: 'cover',
                       p: 2,
                     }}
