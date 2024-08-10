56677abad510d333406fd90ce98ac20d01b7a1b0
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-08-10 18:51:36 +0900

    feat: 뮤직플레이어 실행 시 스낵바, 탑버튼 위치 조정

--- src/components/common/Alert/SnackbarAlert.jsx
+++ src/components/common/Alert/SnackbarAlert.jsx
@@ -1,12 +1,18 @@
 import { Alert, Snackbar } from '@mui/material';
+import useMusicPlayerStore from '../../../store/useMusicPlayerStore';
 
 const SnackbarAlert = ({ open, onClose, message, severity }) => {
+  const { isVisible: isMusicPlayerVisible } = useMusicPlayerStore();
+
   return (
     <Snackbar
       open={open}
       autoHideDuration={6000}
       onClose={onClose}
       anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      sx={{
+        bottom: isMusicPlayerVisible ? 100 : 0,
+      }}
     >
       <Alert onClose={onClose} severity={severity}>
         {message}
