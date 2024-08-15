3636f5a165b84a13e6c86e4fa37d6a20a251f30a
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-08-15 18:35:36 +0900

    chore, feat: 미사용 컴포넌트 삭제, 인기트랙 페이지 로딩중 ui  추가

--- src/pages/tracklist/FeaturedTracks.jsx
+++ src/pages/tracklist/FeaturedTracks.jsx
@@ -1,7 +1,7 @@
 import { getFeaturedPlaylists } from '../../api/track/trackApi';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BaseContainer from '../../components/layout/BaseContainer';
-import { Box, Typography } from '@mui/material';
+import { Box, CircularProgress, Typography } from '@mui/material';
 import TrackListItem from '../../components/common/ListItem/TrackListItem';
 
 const FeaturedTracks = () => {
@@ -11,7 +11,7 @@ const FeaturedTracks = () => {
   const fetchFeaturedTracks = async () => {
     try {
       setLoading(true);
-      const featuredData = await getFeaturedPlaylists('ko_KR', 0, 50); // 6개의 트랙만 가져오기
+      const featuredData = await getFeaturedPlaylists('ko_KR', 0, 50);
       setTracks(featuredData.tracks);
       setLoading(false);
     } catch (error) {
@@ -28,32 +28,42 @@ const FeaturedTracks = () => {
   return (
     <BaseContainer>
       <Typography variant="title">Top 50 Featured Tracks</Typography>
-      <Box sx={{ width: '100%' }}>
-        {tracks.map((track, index) => (
+      <Box sx={{ width: '100%', my: 1 }}>
+        {loading ? (
           <Box
-            key={track.id} // Ensure each Box has a unique key
             sx={{
               display: 'flex',
+              justifyContent: 'center',
               alignItems: 'center',
-              mb: 1,
+              py: 10,
             }}
           >
-            <Typography
-              variant="ranking"
+            <CircularProgress color="secondary" />
+          </Box>
+        ) : (
+          tracks.map((track, index) => (
+            <Box
+              key={track.id} // Ensure each Box has a unique key
               sx={{
-                width: 30,
-                textAlign: 'center',
-                mr: 1,
+                display: 'flex',
+                alignItems: 'center',
+                mb: 1,
               }}
             >
-              {index + 1}
-            </Typography>
-            <TrackListItem music={track} />
-          </Box>
-        ))}
-        {/*{tracks.map(track => (*/}
-        {/*  <TrackListItem music={track} />*/}
-        {/*))}*/}
+              <Typography
+                variant="ranking"
+                sx={{
+                  width: 30,
+                  textAlign: 'center',
+                  mr: 1,
+                }}
+              >
+                {index + 1}
+              </Typography>
+              <TrackListItem music={track} />
+            </Box>
+          ))
+        )}
       </Box>
     </BaseContainer>
   );
